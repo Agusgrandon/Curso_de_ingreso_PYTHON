e5e08f3ea64dc913635eb00e5b93c56a0dec992e
agustina grandon <agustina107.grandon@gmail.com>
2024-02-06 17:07:36 -0300

    agrege ejercicio 3

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: agus
+apellido: grandon
 ---
 Ejercicio: entrada_salida_03
 ---
@@ -34,7 +34,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+       mostrarNombre = self.txt_nombre.get()
+       alert(title="Titulo",message= "Hola"+" "+ mostrarNombre)
                 
     
 if __name__ == "__main__":
